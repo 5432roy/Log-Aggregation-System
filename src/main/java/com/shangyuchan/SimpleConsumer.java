72d6d06a17fee76a1d1ad1af0e6bd8991026fdab
Shang-Yu Chan (Roy) <schan88@ucsc.edu>
2025-03-31 13:42:58 -0700

    `[Add]` A basic structure of a kafka conumser and producer
    - Using Maven for the project
    - Using Docker to run the kafka server

---
+++ src/main/java/com/shangyuchan/SimpleConsumer.java
@@ -0,0 +1,40 @@
+package com.shangyuchan;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class SimpleConsumer {
+    public static void main(String[] args) {
+        // Configure the Consumer
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "test-group");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        
+        String topic = "test-topic";
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList(topic));
+
+        try {
+            System.out.printf("Start listening to: %s%n", topic);
+            while (true) {
+                // Poll for new messages
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.printf("Received message: key = %s, value = %s, offset = %d%n", 
+                                      record.key(), record.value(), record.offset());
+                }
+            }
+        } finally {
+            System.out.printf("Consumer closed%n");
+            consumer.close();
+        }
+    }
+}
---
+++ src/main/java/com/shangyuchan/SimpleProducer.java
@@ -0,0 +1,37 @@
+package com.shangyuchan;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.Producer;
+import java.util.Properties;
+
+public class SimpleProducer {
+    public static void main(String[] args) {
+        // Configure the Producer
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+
+        try {
+            // Send a series of messages to the topic "test-topic"
+            System.out.printf("Start sending messages%n");
+            for (int i = 0; i < 10; i++) {
+                String key = "Key" + i;
+                String value = "Hello Kafka " + i;
+                ProducerRecord<String, String> record = new ProducerRecord<>("test-topic", key, value);
+                producer.send(record);
+                System.out.println("Sent message: " + record.toString());
+            }
+        } finally {
+            producer.close();
+        }
+    }
+}
