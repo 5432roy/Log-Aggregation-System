68238fa39ff2876f6fdd2c59c82084fa717ece6c
Shang-Yu Chan (Roy) <schan88@ucsc.edu>
2025-04-02 14:55:48 -0700

    `[Add]` Producer/Consumer with slf4j library

---
+++ src/main/java/com/shangyuchan/Slf4jConsumer.java
@@ -0,0 +1,66 @@
+package com.shangyuchan;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.Consumer;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Slf4jConsumer {
+    private static final Logger logger = LoggerFactory.getLogger(Slf4jConsumer.class);
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "test-group");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("auto.offset.reset", "earliest");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        String topic = "test-topic";
+        Consumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList(topic));
+
+        Gson gson = new Gson();
+
+        Map<String, Integer> logCounts = new HashMap<>();
+
+        try {
+            System.out.println("Slf4j consumer starts listening to: " + topic);
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record : records) {
+                    try {
+                        LogEntry logEntry = gson.fromJson(record.value(), LogEntry.class);
+                        logCounts.put(logEntry.getLevel(), logCounts.getOrDefault(logEntry.getLevel(), 0) + 1);
+                        logger.info("Received Log {}", logEntry);
+                    } catch (JsonSyntaxException e) {
+                        logger.error("slf4j consumer: Failed to parse log message: {}", record.value() ,e);
+                    }
+                }
+
+                if (!records.isEmpty()) {
+                    logger.info("Aggregated Log Counts: {}", logCounts);
+                }
+            }
+        } catch (Exception e) {
+        } finally {
+            consumer.close();
+            logger.info("slf4j consumer closed");
+        }
+    }
+}
---
+++ src/main/java/com/shangyuchan/Slf4jProducer.java
@@ -0,0 +1,61 @@
+package com.shangyuchan;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.Producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.Random;
+
+public class Slf4jProducer {
+    private static final Logger logger = LoggerFactory.getLogger(Slf4jProducer.class);
+    private static final String TOPIC = "test-topic";
+    private static final String[] SERVICES = {"AuthService", "PaymentService", "OrderService", "NotificationService"};
+    private static final String[] LEVELS = {"INFO", "WARN", "ERROR"};
+    private static final Random random = new Random();
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(props);
+
+        try {
+            System.out.printf("Start sending messages%n");
+            for(int i = 0; i < 10; i++) {
+                String service = SERVICES[random.nextInt(SERVICES.length)];
+                String level = LEVELS[random.nextInt(LEVELS.length)];
+                String message = "Log: " + i + " from " + service;
+
+                LogEntry logEntry = new LogEntry(service, level, message);
+                
+                ProducerRecord<String, String> record = new ProducerRecord<String,String>(TOPIC, service, logEntry.toString());
+                producer.send(record, (metadata, exception) -> {
+                    if(exception != null) {
+                        logger.error("Error sending record to Kafka", exception);
+                    } else {
+                        logger.info("Record sent to partition {} with offset {}", 
+                            metadata.partition(), metadata.offset());
+                    }
+                });
+
+                Thread.sleep(500);
+            }
+        } catch(Exception e) {
+            logger.error("Unexpected error in slf4j procuder", e);
+        } finally {
+            producer.close();
+            logger.info("slf4j producer closed");
+        }
+    }
+}
